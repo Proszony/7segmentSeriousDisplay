23880c7973e5c9992ce5609a7a4f4d3634904bfd
Proszony <elanus04@gmail.com>
2025-11-27 18:10:37 +0100

    ordering

---
+++ cpp/video.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <string>
+
+void run_video(const std::string &filename,
+               float thresh,
+               bool invert_flag,
+               cv::Size res,
+               cv::Size div,
+               float max_fps);
